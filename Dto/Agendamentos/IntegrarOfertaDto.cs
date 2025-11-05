3648564a74694d32da792f87cf8c360f977105e9
Angelo Hervis <angelo.hervis@db1.com.br>
2025-11-05 18:49:00 -0300

    feat: mais uns ajustes por ai

--- Dto/Agendamentos/IntegrarOfertaDto.cs
+++ Dto/Agendamentos/IntegrarOfertaDto.cs
@@ -7,7 +7,6 @@ public class IntegrarOfertaDto
     public string PrecoAtual { get; set; }
     public string PrecoAnterior { get; set; }
     public string ImagemUrlPrincipal { get; set; }
-    public string ImagensUrl { get; set; }
     public string UrlOrigem { get; set; }
     public string Marketplace { get; set; }
 }
